09b7344f0968b99b0d43e9b362d87d712a32f661
marcelinhor <mareginato31@gmail.com>
2024-11-30 20:08:36 -0300

    processamento_dador_edge_fog

--- Edge.py
+++ Edge.py
@@ -2,20 +2,28 @@ import requests
 import random
 import time
 
-FOG_URL = "http://172.25.126.164:5000/process_data"  # Substitua pelo IP da máquina que executa fog.py
+FOG_URL = "http://192.168.118.139:5000/process_data" #Endereço do FOG, para que possamos nos conectar
+UPPER_LIM = 70 #Limite máximo aceitável para o nível de vibração de uma máquina - acima disso precisa de manutenção
 
 def generate_sensor_data():
     return {
-        "temperature": random.uniform(20, 30),
-        "humidity": random.uniform(40, 70)
+        "sensor_id": random.randint(1, 10), #Id definido para cada sensor
+        "sensor_data": random.uniform(0.0, 100.0) #Gerando um dado de sensor, o qual corresponde ao nível de vibração de uma máquina
     }
 
 while True:
-    data = generate_sensor_data()
-    print(f"Sending data: {data}")
-    try:
-        response = requests.post(FOG_URL, json=data)
-        print(f"Response: {response.json()}")
-    except Exception as e:
-        print(f"Error sending data: {e}")
+    data = generate_sensor_data() 
+
+    print("Sensor com id ", data["sensor_id"], ", tem nível de vibração de ", data["sensor_data"])
+
+    if (data["sensor_data"] > UPPER_LIM): #Ultrapassou o limite - envia para a fog
+        print("Nível problemático. Enviando para FOG!")
+        try:
+            requests.post(FOG_URL, json=data)
+        except Exception as e:
+            print(f"Erro ao enviar dado: {e}")
+    else:
+        print("Nível aceitável.")
+
     time.sleep(2)
+
--- Fog.py
+++ Fog.py
@@ -4,16 +4,26 @@ app = Flask(__name__)
 
 @app.route('/process_data', methods=['POST'])
 def process_data():
-    data = request.json
+    data = request.json # Extrai o formato da requisicao HTTP
     print(f"Received data: {data}")
-    # Processamento simples
+    
+    # Processamento de dados simples 
+    classificacao = 'nao classificado'
+
+    if data['sensor_data'] > 70 and data['sensor_data'] < 80:
+        classificacao = 'baixo risco'
+    if data['sensor_data'] > 80 and data['sensor_data'] < 90:
+        classificacao = 'medio risco'
+    if data['sensor_data'] > 90:
+        classificacao = 'alto risco'
+
     processed_data = {
-        "average": (data['temperature'] + data['humidity']) / 2,
-        "original": data
+        "classificacao": classificacao,
     }
+    
     print(f"Processed data: {processed_data}")
     return jsonify(processed_data)
 
 if __name__ == '__main__':
     # Escuta em todas as interfaces da rede local
-    app.run(host='172.25.126.164', port=5000)
+    app.run(host='0.0.0.0', port=5000)
